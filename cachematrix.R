637ccde5b5a344001be1ff7f378f1d2b2564d7f2
srujanasuri <srujana.suri15@gmail.com>
2015-05-14 16:36:21 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The main aim of this script is to read a matrix and to give the inverse of that perticular matrix
+## This script consists of 2 functions "makeCacheMatrix" and "cacheSolve"
 
-## Write a short comment describing this function
+## "makeCacheMatrix()" reads the 'matrix' object that can cache its inverse.
+## It consits of a list of 4 functions 'set', 'get', 'setinv', 'getinv'.
 
 makeCacheMatrix <- function(mtx = matrix()) 
 {
@@ -17,11 +18,14 @@ makeCacheMatrix <- function(mtx = matrix())
       inverse <<- inv;
     }
     getinv <- function() return(inverse);
-    list(set = set, get = get, setinv = setinv, getinv = getinv)
+    return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve()" computes the inverse of the 'matrix' returned by "makeCacheMatrix()" above.
+## If the inverse has already been calculated (and the matrix has not changed), then
+## "cacheSolve()" should retrieve the inverse from the cache.
+
 
 cacheSolve <- function(mtx, ...)  
 {
